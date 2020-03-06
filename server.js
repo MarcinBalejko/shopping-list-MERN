53f4faf11003fcf451eb464119f1ec6960a822dc
MarcinBalejko <mbbm9@o2.pl>
2020-03-07 00:40:43 +0100

    :recycle: static folder

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
+const path = require("path");
 
 const items = require("./routes/api/items");
 
@@ -23,6 +24,16 @@ mongoose
 
 // Use routes
 app.use("/api/items", items);
+
+// Serve static assets if in production
+if (process.env.NODE_ENV == "production") {
+  // Set static folder
+  app.use(experess.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
