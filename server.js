7bd6f01aa72646d2b053e23255592aebaac7ee4d
MarcinBalejko <mbbm9@o2.pl>
2020-03-08 00:07:34 +0100

    :pencil: typos corrected

--- server.js
+++ server.js
@@ -37,4 +37,6 @@ if (process.env.NODE_ENV == "production") {
 
 // const port = process.env.PORT || 5000;
 
-app.listen(PORT, "0.0.0.0", () => console.log("Server up and running"));
+app.listen(PORT, "0.0.0.0", () =>
+  console.log(`Server up and running on port ${PORT}`)
+);
